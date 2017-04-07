3f832f8586bfe552af8739a18d3998b81f15f9cc
Ryan Coyne <ryan@clearcodex.com>
2017-04-07 13:28:25 -0400

    Adding in to send an email verification after signup

--- TrumpPunch/TPSignUpWithEmailViewController.swift
+++ TrumpPunch/TPSignUpWithEmailViewController.swift
@@ -235,7 +235,11 @@ class TPSignUpWithEmailViewController: UIViewController, UITextFieldDelegate {
                     user, error in
                     if error != nil { self.ccxLog(error: error); completion(false, error, nil) }
                     // Lets set the username to the currentUser:
-                    if user != nil { user?.username = username; user?.emailAddress = email; completion(true, nil, nil); }
+                    if user != nil { user?.sendEmailVerification(completion: { (error) in
+                        if error == nil {
+                            // Email verification was sent!
+                        }
+                    }) ;user?.username = username; user?.emailAddress = email; completion(true, nil, nil); }
                 }
             } else {
                 if passwordField.showingError { passwordField.clearErrors() }
